dcee3d6b9602984d717595bc4fcfa0778b299472
Jacob Menke <linux.dev25@gmail.com>
2018-08-23 13:58:23 -0400

    more server configs

--- src/test/java/test1.java
+++ src/test/java/test1.java
@@ -4,8 +4,8 @@ import static org.junit.Assert.assertEquals;
 
 public class test1 {
     @Test
-    public void tonka() {
-        System.err.println("tonka test");
+    public void testme() {
+        System.err.println("testing testme");
         assertEquals(5, 5);
     }
 }
